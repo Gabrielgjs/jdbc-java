a7b8c932022dd31eb9601f6cca333700f07e01f1
gjsgabriel <gjsgabriel83@gmail.com>
2022-08-31 11:05:54 -0300

    inserir dados: Statement Return_generated_keys e getGeneratedKeys

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -25,18 +27,29 @@ public class Program {
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentID) "
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)");
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);//retorna o campo Id do banco de dados. 
 			
-			st.setString(1, "John aryin");
-			st.setString(2, "john@email.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("12/06/1997").getTime()));
-			st.setDouble(4, 2200.00);
-			st.setInt(5, 2);
+			st.setString(1, "arya stark");
+			st.setString(2, "arya@email.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("12/06/1998").getTime()));
+			st.setDouble(4, 3000.00);
+			st.setInt(5, 4);
 			
 			//executedUpdate método para executar o prepareStatement
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done!  Rows affected: " + rowsAffected);
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys(); //retorna o Id como resultset em forma de tabela
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			} else {
+				System.out.println("No rows affected");
+			}
+			
+			
 			
 		} catch(SQLException e) {
 			e.printStackTrace();
